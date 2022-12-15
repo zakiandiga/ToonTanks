628237d8ffd66975636fbd490fa2aeff7790601f
Zak <rusty_clarinet@yahoo.com>
2022-12-15 14:53:33 -0800

    update sounds
    update sounds
    test vs2022 settings

--- Source/ToonTanks/Private/Bullet.cpp
+++ Source/ToonTanks/Private/Bullet.cpp
@@ -1,6 +1,3 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-
 #include "Bullet.h"
 #include "Components/StaticMeshComponent.h"
 #include "GameFramework/ProjectileMovementComponent.h"
